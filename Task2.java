400aa76c5556b59e8cea4aad25712a8292bab888
Mak1ube <semenovrasul7@mail.ru>
2025-03-28 10:21:23 +0500

    Update Task2.java

--- Task2.java
+++ Task2.java
@@ -7,9 +7,11 @@ public class Task2 {
 
     public static void Average(int[] arr, int n) {
         double sum = 0;
+        //Сумируем каждый элемент листа
         for (int i = 0; i < n; i++) {
             sum += arr[i];
         }
+        // Находим среднее
         double average = sum / n;
         System.out.println(average);
     }
