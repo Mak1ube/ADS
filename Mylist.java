b0893e3f67556b4b55a4f6ffa59ae00e1990c181
Mak1ube <semenovrasul7@mail.ru>
2025-04-18 11:20:56 +0500

    Update Mylist.java

--- Mylist.java
+++ Mylist.java
@@ -3,4 +3,18 @@ public interface Mylist<T> {
         void remove(int index);
         T get(int index);
         int size();
+        void set (int index, T item);
+        void add (int index, T item);
+        void addFirst (T item);
+        void addLast (T item);
+        T getFirst ();
+        T getLast ();
+        void removeFirst ();
+        void removeLast ();
+        void clear();
+        void sort();
+        int indexOf (Object object);
+        int lastIndexOf (Object object);
+        boolean exists (Object object);
+        public Object[] toArray();
 }
